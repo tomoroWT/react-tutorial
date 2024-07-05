2347f717c7cbffcd71905806c44e97023d961c84
tomoroWT <chinhighchu@gmail.com>
2024-07-05 15:25:29 +0900

    inputとlabelの関係を修正

--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Todo = ( {todo, toggleTodo} ) => {
-    const handleTodoClick = () => {
-        toggleTodo(todo.id);
-    };
+const Todo = ({ todo, toggleTodo }) => {
+  const handleTodoClick = () => {
+    toggleTodo(todo.id);
+  };
+
+  const inputId = `todo-checkbox-${todo.id}`;
 
   return (
     <div>
-        <label>
-            <input type="checkbox" checked={todo.completed} readOnly onChange={handleTodoClick}/>
-        </label>
+      <input id={inputId} type="checkbox" checked={todo.completed} readOnly />
+      <label onClick={handleTodoClick} htmlFor={inputId}>
         {todo.name}
+      </label>
     </div>
   );
 };
